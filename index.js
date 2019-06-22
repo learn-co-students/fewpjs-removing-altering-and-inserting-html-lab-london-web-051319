ea9970c92b9ac37fff98a13bea3da09d58e901f5
Felix Benetzeder <felix.benetzeder@blackrock.com>
2019-06-22 23:11:13 +0100

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
-// Write your code here!
+let element = document.querySelector('main#main');
+element.remove();
+
+var newHeader = document.createElement('h1');
+newHeader.id = "victory"
+newHeader.innerHTML = "Felix is the champion!";
