6bf66d11ddf6d13b4493578c75809f5979d7c526
moniet <moniet.sawhney@gmail.com>
2019-06-22 18:59:53 +0100

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-// Write your code here!
+let m = document.querySelector('main');
+m.remove();
+
+let newHeader = document.createElement('h1');
+
+newHeader.setAttribute('id', 'victory');
+
+newHeader.innerHTML = 'is the champion!';
