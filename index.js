731526b9c9fbdda5ee2d1d80ffd20de4e96656f9
chrisdews <cldews1@gmail.com>
2019-06-23 19:49:13 +0100

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // Write your code here!
+
+let element = document.querySelector('main#main');
+element.remove();
+
+
+let newHeader = document.createElement(`h1`);
+newHeader.setAttribute("id", "victory");
+newHeader.innerHTML = "Dewsy is the champion";
+document.body.appendChild(newHeader);
