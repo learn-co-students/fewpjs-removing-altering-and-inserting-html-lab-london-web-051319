7eaf52089aa33ce4e54bee2963340df47ca3e4a6
VladTGeorgiev <vlad.georgiev@outlook.com>
2019-06-23 07:17:57 +0100

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
-// Write your code here!
+document.querySelector("main#main").remove();
+
+let newHeader = document.createElement("h1");
+newHeader.id = "victory";
+newHeader.innerHTML = "Dante is the champion";
