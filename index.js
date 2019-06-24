2ac95ca1e942a6f340930a5d73ec8ae6235320c5
ObaldThePedro <pedro.nunes.loureiro@gmail.com>
2019-06-24 11:47:40 +0100

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // Write your code here!
+let element = document.getElementById('main');
+element.remove();
+
+const newHeader = document.createElement('h1');
+newHeader.id = "victory";
+newHeader.innerHTML('Pedro is the champion!');
+document.body.appendChild(newHeader);
